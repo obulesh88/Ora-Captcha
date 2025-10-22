53bc7b6977c253696b073fb7e6925dae6845859f
Obuleswar <obuleswar88@gmail.com>
2025-10-22 06:42:22 +0000

    // next-sitemap.config.js /** @type {import('next-sitemap').IConfig} */

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://ora-captcha.vercel.app/',
+  generateRobotsTxt: true,
+  // other options...
+}
